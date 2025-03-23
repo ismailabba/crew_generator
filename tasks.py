d321eeec8aac7cfeabe2dfc287e37c8d0153f226
ismailabba <ismailamuhammadabba@gmal.com>
2025-03-23 22:11:06 +0100

    task definition

--- tasks.py
+++ tasks.py
@@ -4,38 +4,111 @@ from textwrap import dedent
 
 
 class CustomTasks:
-    def __tip_section(self):
-        return "If you do your BEST WORK, I'll give you a $10,000 commission!"
-
-    def task_1_name(self, agent, var1, var2):
+    
+    def break_goal(self, agent, goal):
         return Task(
-            description=dedent(
-                f"""
-            Do something as part of task 1
+        description=dedent(
+                f""" 
+            ***Task***: Break goal into smaller manageable tasks***
+          
+            ***description***:Given a clear goal divide the goal into smaller manageable tasks 
+               that crew ai agents can execute. 
+               Use this variable: {goal}
+               """
+            ),
             
-            {self.__tip_section()}
-    
-            Make sure to use the most recent data as possible.
-    
-            Use this variable: {var1}
-            And also this variable: {var2}
-        """
-            ),
-            expected_output="The expected output of the task",
+        expected_output=
+          "The expected output of the task should be a list of task whereby each 
+              """item in the list is an object that has task name, description and expected output e.g
+            Task_Name* = Select City
+            Description=dedent(f"""
+
+               """ Analyze and select the best city for the trip based 
+                on specific criteria such as weather patterns, seasonal
+                events, and travel costs. This task involves comparing
+                multiple cities, considering factors like current weather
+                conditions, upcoming cultural or seasonal events, and
+                overall travel expenses. 
+                
+                Your final answer must be a detailed
+                report on the chosen city, and everything you found out
+                about it, including the actual flight costs, weather 
+                forecast and attractions. """
+       
+            """),
+            expected_output="Detailed report on the chosen city including flight costs, weather forecast, and attractions" """
+
+            ,
             agent=agent,
         )
 
-    def task_2_name(self, agent):
+
+
+
+
+
+    def assign_task(self, agent, tasks):
         return Task(
+         
             description=dedent(
-                f"""
-            Take the input from task 1 and do something with it.
-                                       
-            {self.__tip_section()}
+                f""" 
+            ***Task***: Map crew AI agents to the provided task list***
+          
+            ***description***:Given the task list Map crew AI agents to the provided task list, defining each agent's responsibility, goal, and motivational context. Provide a concise agent profile, 
+            including their professional history, relevant skills, and expertise
 
-            Make sure to do something else.
-        """
+             Use this variable: {tasks}
+            """
             ),
-            expected_output="The expected output of the task",
+            
+            expected_output=
+            """The expected output of the task should be an array of 
+            crewAi agent responsible for each of the task on the list
+            an agent should like this.
+
+            Agent(
+                role='Local Expert at this city',
+                goal='Provide the BEST insights about the selected city',
+                backstory= A knowledgeable local guide with extensive information
+                about the city, it's attractions and customs,)
+            """
+         ,
             agent=agent,
         )
+
+
+
+    def assign_tool(self, agent, agentsList):
+        return Task(
+                  description=dedent(
+                f""" 
+            ***Task***: Assign necessary tools to agents***
+          
+            ***description***:Given the crewai agent list Allocate the necessary tools to each agent, 
+            ensuring they have the required resources to complete their assigned tasks. 
+            Specify the tools needed for each agent to execute their task
+
+             Use this variable: {agentsList}
+            """
+            
+            
+            ),
+               expected_output=
+            """The expected output of the task should be an array of 
+            crewAi agent responsible for each of the task on the list with 
+            tools they might need to accomplish the task
+            an agent should like this.
+
+            Agent(
+                role='Local Expert at this city',
+                goal='Provide the BEST insights about the selected city',
+                backstory= A knowledgeable local guide with extensive information
+                about the city, it's attractions and customs,
+                tools=[
+                SearchTools.search_internet,
+                BrowserTools.scrape_and_summarize_website,
+                ],
+            """,
+            agent=agent,
+        )
+ 
